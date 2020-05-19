9e214b09f76bee6edd43fdd178a00579cd0c28ad
madisonwong210 <58918746+madisonwong210@users.noreply.github.com>
2020-05-18 21:06:20 -0700

    created fake comments

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -61,6 +61,25 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a random comment"
+        comment["author"] = PFUser.current()!
+        comment["post"] = post
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print ("comment saved")
+            } else {
+                print("error saving comment")
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
